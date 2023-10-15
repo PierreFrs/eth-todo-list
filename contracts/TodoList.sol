142ac436b14c482c715429814d82fb379eb23bbe
PierreFrs <pierrefraisse90@gmail.com>
2023-10-15 11:52:34 +0200

    truffle init + new migration

---
+++ contracts/TodoList.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT 
+// Declare Solidity version
+pragma solidity ^0.5.0;
+
+// Declare the contract, all is written between {}
+contract TodoList {
+    uint public taskCount = 0;
+}
