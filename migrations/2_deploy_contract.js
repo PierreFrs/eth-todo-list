a6e864fbea06f52deeb5b9c215a644f0ca2ab456
PierreFrs <pierrefraisse90@gmail.com>
2023-10-15 16:43:00 +0200

    TodoList migration

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+// Smart contract abstraction => put/deploy in the block chain
+
+const TodoList = artifacts.require("./TodoList.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(TodoList);
 };
